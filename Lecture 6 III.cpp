877f5f86c2751ddb37c057fb7415dca7c11ff6de
Abhinav <abhi9780244826@gmail.com>
2023-10-06 22:55:15 +0530

    LECTURE 6 III

---
+++ Lecture 6 III.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int inputs;
+    int i;
+   for(i=1;i<101;i++){
+    cin>>inputs;
+    if(inputs==65){
+        cout<<"Congrats you have guessed correct";
+        break;
+    }
+   }
+}
